bd23acc6c5fd3f8e471e55e294d493925ec1c08a
Bruno Mosconi <contato.brunomosconi@gmail.com>
2019-06-02 22:44:10 +0000

    Matheus: Alteração tela de usuario logado

--- src/Handler/HomeLogado.hs
+++ src/Handler/HomeLogado.hs
@@ -20,16 +20,41 @@ getHomeLogadoR = do
         -- pasta: static/css/bootstrap.css
         -- / e . sao trocados por _
         addStylesheet $ StaticR css_bootstrap_css
+        
         toWidgetHead [julius|
-            function teste(){
-                alert("BORA JOGAR!");
-            }
         |]
+        
         toWidget [lucius|
+            body {
+                background: rgb(173,216,230);
+                background: linear-gradient(90deg, rgba(173,216,230,1) 0%, rgba(255,255,255,0) 20%, rgba(242,249,251,1) 80%, rgba(173,216,230,1) 100%);            }
+    
             h1{
-                color : orange;
+                color : green;
                 text-align: center;
                 margin-top:50px;
+            }
+            div{
+                align-items: center;
+                display: flex;
+                flex-direction:row;
+                flex-wrap: wrap;
+                justify-content: center;
+            }
+            
+            #init{
+                float:left;
+                widght:100px;
+                height:150px;
+            }
+          
+            ul{
+                display: flex;            
+                flex-direction:row; 
+            }
+            
+            span{
+                align: center;
             }
           
             #end{
@@ -37,75 +62,49 @@ getHomeLogadoR = do
                 widght:100px;
                 height:150px;
             }
-             #init{
-                float:left;
-                widght:100px;
-                height:150px;
-            }
-            span{
-                align: center;
-            }
-            
-            a{
-                 margin:20px;
-            }
         |]
+        
         [whamlet|
             $maybe sessao <- sess    
                 Ola #{sessao}
-            $nothing    
+            $nothing 
+            
             <div class="container">
-                <h1 class>
-                    <img src=@{StaticR imgs_boraJogar_jpg} id="init">
+                <img src=@{StaticR imgs_boraJogar_jpg} id="init">
+                
+                <h2 class="h2">
+                    Bem Vindo ao Bora Jogar
                     
-                    <span>
-                        Bem Vindo ao Bora Jogar
-                        
-                    <img src=@{StaticR imgs_boraJogar_jpg} id="end">  
+                <img src=@{StaticR imgs_boraJogar_jpg} id="end">  
                     
         
-                    
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href=@{HomeR}>
-                    Inicial
-                <div class="collapse navbar-collapse id="navbarNavAltMarkup>
-                
-                
-                    <div class="navbar-nav">
-               
-                       
-                            <a href=@{AtletaR} class="nav-link" >
-                                Cadastro de Atletas
-                       
-                            <a href=@{TodosAtletasR} class="nav-link" >
-                                Listar Atletas
-                        
-                            <a href=@{LocalR} class="nav-link" >
-                                Cadastro de Locais
-                        
-                            <a href=@{TodosLocaisR} class="nav-link" >
-                                Listar Locais
-                       
-                            <a href=@{EsporteR} class="nav-link" >
-                                Cadastro de Esportes
-                      
-                            <a href=@{TodosEsportesR} class="nav-link" >
-                                Listar Esportes
-                       
-                            <a href=@{EventoR} class="nav-link" >
-                                Cadastro de Eventos
-                       
-                            <a href=@{TodosEventosR} class="nav-link" >
-                                Listar Eventos
-                
+            <br>
+            <br>
+            <br>
+
+               <ul class="nav nav-tabs">
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{AtletaR}">Cadastro de Atletas</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{TodosAtletasR}">Atletas Cadastrados</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{LocalR}">Cadastro de Locais</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{TodosLocaisR}">Locais  Cadastrados</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{EsporteR}">Cadastro de Esportes</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{TodosEsportesR}">Esportes Cadastrados</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{EventoR}">Cadastro de Eventos</a>
+                  <li class="nav-item">
+                    <a class="nav-link" href="@{TodosEventosR}">Eventos Cadastrados</a>
+                  <li class="nav-item">
+
             $maybe _ <- sess 
                        
                 <form action=@{LogoutR} method=post>
                     <input type="submit" value="Sair">
             $nothing  
                
-               
-                        
-            
-            
         |]
